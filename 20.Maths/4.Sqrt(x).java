632af674599fcfa0a6cbc3a6bc6fb022f8dbb703
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-03-08 12:16:08 +0530

    Update 4.Sqrt(x).java

--- 20.Maths/4.Sqrt(x).java
+++ 20.Maths/4.Sqrt(x).java
@@ -1,12 +1,9 @@
-class Solution { 
+class Solution {
     public int mySqrt(int x) {
         if(x==0){
             return 0;
         }
-        int low=1;
-        int high=x;
-        int result=0;
-
+        int low=1;int high=x;int result=0;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(mid<=x/mid){
