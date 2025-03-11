bbcf10e26b9047a5c665ce4f1055dbabecd70c42
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-03-11 06:35:47 +0530

    Update 1.Merge Sorted Array.java

--- 1.Array&String/1.Merge Sorted Array.java
+++ 1.Array&String/1.Merge Sorted Array.java
@@ -1,3 +1,18 @@
+(this is approach  not recommended for interview )
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
+
+
+
+
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int midx = m - 1;
