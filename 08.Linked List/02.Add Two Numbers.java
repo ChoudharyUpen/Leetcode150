8c0129a18e727fc4f605447dee03080fe5f5c112
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-03-13 11:46:36 +0530

    Update 02.Add Two Numbers.java

--- 08.Linked List/02.Add Two Numbers.java
+++ 08.Linked List/02.Add Two Numbers.java
@@ -1,3 +1,15 @@
+Input:
+l1 = [2 → 4 → 3] (342)
+l2 = [5 → 6 → 4] (465)
+
+l1	l2	Carry	Sum	New Node	Carry Forward
+2	5	0	     2+5=7	7	0
+4	6	0	    4+6=10	0	1
+3	4	1	   3+4+1=8	8	0
+Result: [7 → 0 → 8] (807) ✅
+
+
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy =new ListNode();
