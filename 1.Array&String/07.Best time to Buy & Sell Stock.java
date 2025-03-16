5d99523999ebae512d0df011093fa1ac964c1447
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-03-16 10:44:36 +0530

    Update 07.Best time to Buy & Sell Stock.java

--- 1.Array&String/07.Best time to Buy & Sell Stock.java
+++ 1.Array&String/07.Best time to Buy & Sell Stock.java
@@ -1,5 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int profit=0;
+        int maxprofit=0;
+        int minSofar=prices[0];
+        
+        for(int i=1;i<prices.length;i++){
+            minSofar=Math.min(minSofar,prices[i]);
+            profit=prices[i]-minSofar;
+            maxprofit=Math.max(profit,maxprofit);
+        }
+        return maxprofit;
+    }
+}
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+    public int maxProfit(int[] prices) {
         int maxProfit=0;
         int minSofar=prices[0];
 
