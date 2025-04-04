a99f679ad9d2a48f1377be269bb5f04d42f1ba14
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-04-04 21:32:16 +0530

    Update 17. Roman to Integer.java

--- 1.Array&String/17. Roman to Integer.java
+++ 1.Array&String/17. Roman to Integer.java
@@ -1,6 +1,5 @@
 class Solution {
     public int romanToInt(String s) {
-       
          int ans = 0, num = 0;
         for (int i = s.length()-1; i >= 0; i--) {
             switch(s.charAt(i)) {
@@ -12,10 +11,10 @@ class Solution {
                 case 'D': num = 500; break;
                 case 'M': num = 1000; break;
             }
+            //chhoti value ke baad badi value thi → subtract karna hai
             if (4 * num < ans) ans -= num;
             else ans += num;
         }
         return ans;
-    }
-    
+    } 
 }
