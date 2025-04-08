558b653232f54df3b04881fe57b1988c23e20c06
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-04-08 19:50:52 +0530

    Create 20.Longest Common Prefix.java

---
+++ 1.Array&String/20.Longest Common Prefix.java
@@ -0,0 +1,28 @@
+🔥 Super Simple Recap:
+1.Strings ko sort karo
+2.First aur last string lo
+3.Dono ke starting se compare karo
+4.Jahan tak match ho raha hai, wahi return kar do
+
+
+
+
+
+
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
+        while(idx < s1.length() && idx < s2.length()){
+            if(s1.charAt(idx) == s2.charAt(idx)){
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, idx);
+    }
+}
