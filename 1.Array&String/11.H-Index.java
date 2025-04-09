92e29af7613cc49de40844d769c62ed30e23db57
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-04-09 13:41:30 +0530

    Update 11.H-Index.java

--- 1.Array&String/11.H-Index.java
+++ 1.Array&String/11.H-Index.java
@@ -1,3 +1,11 @@
+🪄 Super Simple Summary:
+1.Citations ko sort karo.
+2.Check karo har paper ke liye:
+3.Kya citation[i] >= bache hue papers (n - i)?
+4.Agar haan, to n - i hi answer.
+5.Agar koi bhi na mila toh return 0.
+
+
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
